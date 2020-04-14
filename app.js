a376492b66155347e0bf87085617f38e3a774419
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-04-14 11:07:50 +0500

    Add html markup

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'client')));
+app.use(express.static(path.resolve(__dirname, 'client'))); // Делаем папку статической, тк в index.html идет вызов ф-ла frontend.js по ОТНОСИТЕЛЬНОМУ ПУТИ
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
 });
