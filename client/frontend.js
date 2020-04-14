f408234b6da3336e50cf471b6ce2d6ca7e089a0e
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-04-14 16:55:06 +0500

    ADD, REMOVE and Marked local files

--- client/frontend.js
+++ client/frontend.js
@@ -11,6 +11,8 @@ new Vue({
 			},
 			contacts: [
 				{ id: 1, name: 'Эльмира', value: '3 Августа', marked: false },
+				{ id: 2, name: 'Тимур', value: '17 Января', marked: false },
+				{ id: 3, name: 'Рената', value: '22 Ноября', marked: false },
 			],
 		};
 	},
@@ -24,16 +26,16 @@ new Vue({
 			// Деструктуризируем данные при помощи спред оператора
 			const { ...contact } = this.form;
 			console.log('Date.now() = ', Date.now());
-			this.contacts.push({ ...contact, id: Date.now() });
+			this.contacts.push({ ...contact, id: Date.now(), marked: false });
 			// Очистим поля формы
 			this.form.name = this.form.value = '';
 		},
 		markContact(id) {
-			const contact = this.contacts.find((c) => c.id === id);
+			const contact = this.contacts.find((c) => c.id === id); // Находим элемент contacts чтобы отметить его как маркерованный
 			contact.marked = true;
 		},
 		deleteContact(id) {
-			console.log('delete = ', id);
+			this.contacts = this.contacts.filter((c) => c.id !== id); // Filter возвращает новый массив, который не  содержит элемента с данным ID
 		},
 	},
 });
